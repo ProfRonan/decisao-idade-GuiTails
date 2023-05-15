e49894205532bae11779a933333f4c4b974f1a4d
Ronan Pardo Soares <ronan.soares@gmail.com>
2023-05-15 15:59:06 -0300

    Fix change of tests

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ print("Digite um número")
 número = int(input("> "))
 
 if número < 0:
-    print("impossível!")
+    print("Impossível!")
 if número < 18:
-    print("não precisa se alistar.")
+    print("Não precisa se alistar.")
 if número > 18: 
     print("Não esqueça de votar na próxima eleição.")
 if número > 65:
     print("Vá descansar.")
 else: 
-    print("eita!")
+    print("Eita!")
