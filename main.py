1260dc20b472bcf5d38c44e9348d767d72fedfb0
Guilherme <128387905+GuiTails@users.noreply.github.com>
2023-05-24 11:18:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 print("Digite um número")
 número = int(input("> "))
-
+X = False
 if número < 0:
     print("Impossível!")
-if número < 18:
+    X = True
+if número < 18 and número >=0:
     print("Não precisa se alistar.")
-if número > 18: 
+    X = True
+if número > 18 and número < 65: 
     print("Não esqueça de votar na próxima eleição.")
+    X = True
 if número > 65:
     print("Vá descansar.")
-else: 
+    X = True
+if X is False:
     print("Eita!")
